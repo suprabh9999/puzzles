3a25f03fb230660acee1742895ebacda32aef5e4
Mona Lisa <official.suprabh@gmail.com>
2019-05-03 15:12:18 +0530

    matrix

--- puzzles/arrays/2D_Array/matrix.js
+++ puzzles/arrays/2D_Array/matrix.js
@@ -1,3 +1,19 @@
+/* 
+    Question -  Transpose the given N x N ( where N represents number of 
+        rows and columns ) matrix.
+    Input matix - 4 x 4 
+        [ 1, 5, 7, 9 ]
+        [ 2, 6, 8, 1 ]
+        [ 3, 9, 4, 6 ]
+        [ 1, 2, 3, 4 ]
+
+    Output matrix - 4 x 4 
+        [ 1, 2, 3, 1 ]
+        [ 5, 6, 8, 2 ]
+        [ 7, 9, 4, 3 ]
+        [ 9, 1, 6, 4 ]
+*/
+
 ( function transposeMatrix(){
     const inputMatrix = [[1, 5, 7, 9],
                          [2, 6, 8, 1],
